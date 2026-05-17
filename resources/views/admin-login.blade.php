4dfe04469fe3f462df8c9d32fd861d56dc358a14
developer-vkp <developer@vkp.co.in>
2026-05-17 17:48:50 +0530

    Favicon

--- resources/views/admin-login.blade.php
+++ resources/views/admin-login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login - VKP Work Monitor</title>
+    <link rel="icon" type="image/png" href="{{ asset('storage/images/logo.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <style>
         * {
